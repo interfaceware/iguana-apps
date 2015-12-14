ac96c7f8460bd374afc8be4ee26687618afc2512
eliot.muir <eliot.muir@interfaceware.com>
2015-12-14 12:05:37 -0500

    Removing shared dependencies.

--- other/app/cm/settings.js
+++
@@ -1,2 +0,0 @@
-cm.settings = {}
-cm.settings.repository = 0;
--- shared/node.lua
+++
@@ -1,8 +0,0 @@
--- Utilities for "node" Values
-
--- Coerce a node value into a string.
---
-function node:S()
-   return tostring(self)
-end
-
